887e3fa5b19ca34f14bf36475e1744c0cd08981f
Francesco Antoniazzi <francesco.antoniazzi1991@gmail.com>
2017-02-21 15:18:58 +0100

    standard ttyUSB0

--- main.c
+++ main.c
@@ -146,7 +146,8 @@ int main(int argc, char ** argv) {
 		ridSerial.dataBits = CS8;
 		ridSerial.parityBit = NO_PARITY;
 		ridSerial.stopbits = ONE_STOP;
-		ridSerial_descriptor = open_serial("",ridSerial);
+		// ridSerial_descriptor = open_serial("/dev/ttyUSB0",ridSerial);
+		ridSerial_descriptor = open_serial("/dev/ttyUSB0",ridSerial);
 		if (ridSerial_descriptor == ERROR) return EXIT_FAILURE;
 
 		// writes to serial "+\n"
