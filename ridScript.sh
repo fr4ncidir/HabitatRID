b2b81c7de03fd05d390f375fd40c18f9149e7983
Francesco Antoniazzi <francesco.antoniazzi1991@gmail.com>
2017-03-02 08:34:27 +0100

    script improved

--- ridScript.sh
+++ ridScript.sh
@@ -1,48 +1,75 @@
-#! /bin/sh
+#! /bin/bash
 
 # Author Francesco Antoniazzi
 # francesco.antoniazzi@unibo.it
 
 case $# in
 	0)
-		cat README
-		exit 0
+		# shows help
+		more README
 		;;
 	1)
 		if [ $1 == "help" ]; then
-			cat README
-			exit 0
+			# shows help
+			more README
 		elif [ $1 == "setup" ]; then
+			# install requirements
+			echo System setup...
 			sudo apt-get install libgsl0ldbl gsl-doc-info gsl-bin libgsl0-dev
 		elif [ $1 == "run" ]; then
-			./RIDexecutable.exe
-		elif [ $1 == "simulate" ]; then
-			
+			# runs executable generated: check if executable exists and has permissions
+			if [ -x "./RIDexecutable.exe" ]; then
+				./RIDexecutable.exe
+			else
+				echo Error: RIDexecutable.exe not found or has not exe permissions
+				exit 7
+			fi
 		else
-			cat README
+			# error case
+			echo Unexpected param $1...
+			more README
 			exit 3
 		fi
 		;;
 	2)
 		if [ $1 == "compile" ]; then
+			# compile case
 			if [ -a $2 ]; then
+				# check if USB port exists and compile, give permissions
 				echo Setting USB port name $2...
-				sed -i "s|/dev/ttyUSB0|$1|g" main.c
+				sed -i "s|/dev/ttyUSB0|$2|g" main.c
 				echo Compiling files...
 				gcc -Wall -I/usr/local/include main.c RIDLib.c serial.c -o RIDexecutable.exe -lgsl -lgslcblas -lm
 				chmod 777 RIDexecutable.exe
+				sed -i "s|$2|/dev/ttyUSB0|g" main.c
 			else
+				# USB port does not exist
 				echo Error: $2 doesn\'t exists
 				exit 5
 			fi
+		elif [ $1 == "simulate" ]; then
+			# simulation case
+			if [ -a $2 ]; then
+				# check if simulation input file exists
+				echo RID simulation...
+				cat /etc/issue | python ridSimulation.py $2 
+			else
+				# input format file does not exist
+				echo Error: $2 doesn\'t exists
+				exit 6
+			fi
 		else
-			cat README
+			# error case
+			echo Unexpected param $1...
+			more README
 			exit 4
 		fi
 		;;
 	*)
+		# default error case
 		echo Too much parameters
-		cat README
+		more README
 		exit 2
 		;;
 esac
+exit 0
