402a4a037d22327900a35c80738f8c36eeda797b
Francesco Antoniazzi <francesco.antoniazzi1991@gmail.com>
2018-01-23 13:41:48 +0100

    tolte robe inutili dal repo

--- setup/setup_raspberry.sh
+++ setup/setup_raspberry.sh
@@ -6,7 +6,7 @@ cd projects
 git clone https://github.com/vaimee/sepa-C-kpi.git
 git clone https://github.com/warmcat/libwebsockets.git
 git clone https://github.com/fr4ncidir/SerialLib.git
-sudo apt-get install figlet cmake libssl-dev libcurl4-gnutls-dev libglib2.0-dev python-pip libgsl0-dev
+sudo apt-get install figlet cmake libssl-dev libcurl4-gnutls-dev libglib2.0-dev python-pip libgsl0-dev socat
 cd ./libwebsockets
 mkdir build
 cd build
